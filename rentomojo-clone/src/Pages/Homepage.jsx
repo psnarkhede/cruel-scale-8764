52e1500e7af1943b9a4dfb7d257bd718573926ed
Rahul Yadav <96325446+yadav9452@users.noreply.github.com>
2022-07-20 18:20:46 +0530

    crousel Added

--- rentomojo-clone/src/Pages/Homepage.jsx
+++ rentomojo-clone/src/Pages/Homepage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Carousel from "../Components/Crousel";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { Box } from "@chakra-ui/react";
 
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <Box>
+      <Carousel />
+      <BsFillChatQuoteFill
+        // marginTop="20px"
+        size={"60px"}
+        right="side"
+        top="0"
+        bottom="0"
+      />
+    </Box>
+  );
 };
 
 export default Homepage;
