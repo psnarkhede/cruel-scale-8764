6d70a4e2a5a0b091ff7122a101a4b064860f240d
raushan kuamr <sraushan890@gmail.com>
2022-07-21 03:22:15 +0530

    header part completed

--- rentomojo-clone/src/Pages/Cartpage.jsx
+++ rentomojo-clone/src/Pages/Cartpage.jsx
@@ -1,8 +1,21 @@
+import { Box, Flex, Stack } from '@chakra-ui/react'
 import React from 'react'
+import { CartHeader } from '../Components/CartHeader'
 
 const Cartpage = () => {
   return (
-    <div>Cartpage</div>
+    <Box border='2px solid #1A202C' w='100%' h='100vh' bg='#EDF2F7'>
+      <Stack direction='column'>
+        <Flex direction='column'>
+           <Box>
+             <CartHeader/>
+           </Box>
+           <Box>
+             {/* <CartHeader/> */}
+           </Box>
+        </Flex> 
+      </Stack>
+    </Box>
   )
 }
 
