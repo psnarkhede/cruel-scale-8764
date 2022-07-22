fa84b0e18fc85ccf8bdbbedb398cd1d03effd590
Rahul Yadav <96325446+yadav9452@users.noreply.github.com>
2022-07-22 21:13:02 +0530

    sliderHome

---
+++ rentomojo-clone/src/Components/SliderHome.jsx
@@ -0,0 +1,47 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineLeftCircle, AiOutlineRightCircle } from "react-icons/ai";
+
+const SliderHome = () => {
+  return (
+    <Box
+      //  border={"1px solid red"}
+      width={"85%"}
+      margin="auto"
+    >
+      <Box textAlign={"start"} marginTop="100px">
+        <Flex flexDirection={"row"} justifyContent="space-between">
+          <Box>
+            <Text
+              fontFamily={"Muli, sans-serif"}
+              fontSize="30px"
+              fontWeight={"bold"}
+            >
+              You'll love to
+            </Text>
+            <Text
+              fontFamily={"Muli, sans-serif"}
+              fontSize="30px"
+              color={"grey"}
+            >
+              take these home
+            </Text>
+            <Box width="50px"></Box>
+          </Box>
+          <Box marginTop={"50px"}>
+            <Flex>
+              <AiOutlineLeftCircle size="50" color="grey" />
+              <AiOutlineRightCircle
+                size="50"
+                color="grey"
+                fontVariant={"red"}
+              />
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default SliderHome;
